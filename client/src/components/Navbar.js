0bc2d1201dd6edf44e42c2d215957aac5eec3a42
JGarcia1998 <jared_garcia@aol.com>
2020-09-04 14:25:09 -0500

    side menu in progress

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { useState } from "react";
 
 function Navbar() {
+  const [Class, setClass] = useState("fa fa-bars");
+  // "fa fa-times" "fa fa-bars"
+  const changeClass = () => {
+    if (Class == "fa fa-bars") {
+      setClass("fa fa-times");
+    } else {
+      setClass("fa fa-bars");
+    }
+  };
+
   return (
     <div className="navbar-container">
       <img src="logo192.png"></img>
@@ -9,6 +20,11 @@ function Navbar() {
         <input type="text" placeholder="Search City or Zip Code" />
         <button>
           <i class="fa fa-search" aria-hidden="true"></i>
+        </button>
+      </div>
+      <div className="bars-icon">
+        <button onClick={changeClass}>
+          <i class={Class} aria-hidden="true"></i>
         </button>
       </div>
     </div>
