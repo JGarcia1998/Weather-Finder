dfd5ee9891082914034bb8e12b0a7e5a751fefd5
JGarcia1998 <jared_garcia@aol.com>
2020-09-04 14:47:58 -0500

    added text decoration

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,12 +3,15 @@ import { useState } from "react";
 
 function Navbar() {
   const [Class, setClass] = useState("fa fa-bars");
+  const [showMenu, setShowMenu] = useState(false);
   // "fa fa-times" "fa fa-bars"
   const changeClass = () => {
     if (Class == "fa fa-bars") {
       setClass("fa fa-times");
+      setShowMenu(true);
     } else {
       setClass("fa fa-bars");
+      setShowMenu(false);
     }
   };
 
@@ -29,42 +32,44 @@ function Navbar() {
           </button>
         </div>
       </div>
-      <div className="navbar-opening">
-        <ul className="listing-container">
-          {" "}
-          WEATHER
-          <li>National Forecast</li>
-          <li>Monthly Forecast</li>
-          <li>Early Forecast</li>
-          <li>Daily Forecast</li>
-          <li>National News</li>
-          <li>Almanac</li>
-        </ul>
-        <ul className="listing-container">
-          {" "}
-          RADAR
-          <li>Weather in Motion</li>
-          <li>Radar Maps</li>
-          <li>Classic Weather Maps</li>
-          <li>Regional Satellite</li>
-        </ul>
-        <ul className="listing-container">
-          {" "}
-          SEVERE
-          <li>Sever Alerts</li>
-          <li>Safety & Prepardness</li>
-          <li>Hurricane Control</li>
-        </ul>
-        <ul className="listing-container">
-          {" "}
-          VIDEO & PHOTOS
-          <li>Top Stories</li>
-          <li>Video</li>
-          <li>Slideshows</li>
-          <li>Climate News</li>
-          <li>Award-Winning Investigations</li>
-        </ul>
-      </div>
+      {showMenu ? (
+        <div className="navbar-opening">
+          <ul className="listing-container">
+            {" "}
+            WEATHER
+            <li>National Forecast</li>
+            <li>Monthly Forecast</li>
+            <li>Early Forecast</li>
+            <li>Daily Forecast</li>
+            <li>National News</li>
+            <li>Almanac</li>
+          </ul>
+          <ul className="listing-container">
+            {" "}
+            RADAR
+            <li>Weather in Motion</li>
+            <li>Radar Maps</li>
+            <li>Classic Weather Maps</li>
+            <li>Regional Satellite</li>
+          </ul>
+          <ul className="listing-container">
+            {" "}
+            SEVERE
+            <li>Sever Alerts</li>
+            <li>Safety & Prepardness</li>
+            <li>Hurricane Control</li>
+          </ul>
+          <ul className="listing-container">
+            {" "}
+            VIDEO & PHOTOS
+            <li>Top Stories</li>
+            <li>Video</li>
+            <li>Slideshows</li>
+            <li>Climate News</li>
+            <li>Award-Winning Investigations</li>
+          </ul>
+        </div>
+      ) : null}
     </>
   );
 }
