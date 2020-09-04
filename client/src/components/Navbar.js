e64a078d82dae51890dff7adb5f5c7e64301e16e
JGarcia1998 <jared_garcia@aol.com>
2020-09-04 14:44:20 -0500

    adding content

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,21 +13,59 @@ function Navbar() {
   };
 
   return (
-    <div className="navbar-container">
-      <img src="logo192.png"></img>
+    <>
+      <div className="navbar-container">
+        <img src="logo192.png"></img>
 
-      <div className="navbar-children">
-        <input type="text" placeholder="Search City or Zip Code" />
-        <button>
-          <i class="fa fa-search" aria-hidden="true"></i>
-        </button>
+        <div className="navbar-children">
+          <input type="text" placeholder="Search City or Zip Code" />
+          <button>
+            <i class="fa fa-search" aria-hidden="true"></i>
+          </button>
+        </div>
+        <div className="bars-icon">
+          <button onClick={changeClass}>
+            <i class={Class} aria-hidden="true"></i>
+          </button>
+        </div>
       </div>
-      <div className="bars-icon">
-        <button onClick={changeClass}>
-          <i class={Class} aria-hidden="true"></i>
-        </button>
+      <div className="navbar-opening">
+        <ul className="listing-container">
+          {" "}
+          WEATHER
+          <li>National Forecast</li>
+          <li>Monthly Forecast</li>
+          <li>Early Forecast</li>
+          <li>Daily Forecast</li>
+          <li>National News</li>
+          <li>Almanac</li>
+        </ul>
+        <ul className="listing-container">
+          {" "}
+          RADAR
+          <li>Weather in Motion</li>
+          <li>Radar Maps</li>
+          <li>Classic Weather Maps</li>
+          <li>Regional Satellite</li>
+        </ul>
+        <ul className="listing-container">
+          {" "}
+          SEVERE
+          <li>Sever Alerts</li>
+          <li>Safety & Prepardness</li>
+          <li>Hurricane Control</li>
+        </ul>
+        <ul className="listing-container">
+          {" "}
+          VIDEO & PHOTOS
+          <li>Top Stories</li>
+          <li>Video</li>
+          <li>Slideshows</li>
+          <li>Climate News</li>
+          <li>Award-Winning Investigations</li>
+        </ul>
       </div>
-    </div>
+    </>
   );
 }
 
